7c5ac4ae84a4190bd6abeb555aeae19fe0d74dda
Izabela <155007065+izafit@users.noreply.github.com>
2025-02-24 19:20:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,34 +2,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
-const fs = require('fs');
 
-// read json file
-let comments = JSON.parse(fs.readFileSync('comments.json'));
-
-// create body-parser
-app.use(bodyParser.json());
-
-// get all comments
-app.get('/comments', (req, res) => {
-    res.send(comments);
+// get request
+app.get('/', (req, res) => {
+    res.send("Hello world");
 });
 
-// get comment by id
-app.get('/comments/:id', (req, res) => {
-    const id = req.params.id;
-    const comment = comments.find(comment => comment.id == id);
-    if (comment) {
-        res.send(comment);
-    } else {
-        res.status(404).send('Comment not found');
-    }
+// start server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-// add new comment
-app.post('/comments', (req, res) => {
-    if (req.body.text && req.body.author) {
-        const comment = {
-            id: comments.length + 1,
-            text: req.body.text,
