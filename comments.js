91d7821c3cad194a116130c04d9cbfa7780edfa5
Izabela <155007065+izafit@users.noreply.github.com>
2025-02-24 19:15:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,50 +1,35 @@
-// create web serwer
+// create web serwer with express
 const express = require('express');
 const app = express();
+const port = 3000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const cors = require('cors');
 
+// read json file
+let comments = JSON.parse(fs.readFileSync('comments.json'));
+
+// create body-parser
 app.use(bodyParser.json());
-app.use(cors());
 
+// get all comments
 app.get('/comments', (req, res) => {
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log('Error reading file from disk:', err);
-            return;
-        }
-        try {
-            const comments = JSON.parse(data);
-            res.send(comments);
-        } catch (err) {
-            console.log('Error parsing JSON string:', err);
-        }
-    });
+    res.send(comments);
 });
 
+// get comment by id
+app.get('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    const comment = comments.find(comment => comment.id == id);
+    if (comment) {
+        res.send(comment);
+    } else {
+        res.status(404).send('Comment not found');
+    }
+});
+
+// add new comment
 app.post('/comments', (req, res) => {
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log('Error reading file from disk:', err);
-            return;
-        }
-        try {
-            const comments = JSON.parse(data);
-            comments.push(req.body);
-            fs.writeFile('comments.json', JSON.stringify(comments), 'utf8', (err) => {
-                if (err) {
-                    console.log('Error writing file:', err);
-                    return;
-                }
-            });
-            res.send(comments);
-        } catch (err) {
-            console.log('Error parsing JSON string:', err);
-        }
-    });
-});
-
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-});
+    if (req.body.text && req.body.author) {
+        const comment = {
+            id: comments.length + 1,
+            text: req.body.text,
